5804e3d11177ea87fb6ebe30f12952b18e9f8269
kaanertugrulkoc <poison_314@hotmail.com>
2025-03-09 21:48:52 +0300

    güncellendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material',
+      title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('--BÜTÇE--'),
+          title: Text('Material App Bar'),
         ),
-        body: const Center(
-          child: Text(''),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
         ),
       ),
     );
---
+++ lib/main1.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: const Text('BÜTÇE')),
+        ),
+        body: const Center(
+          child: Text(''),
+        ),
+      ),
+    );
+  }
+}
