ac40ebe7b5a714f10545f26da23f3af5f62ac1f2
kaanertugrulkoc <poison_314@hotmail.com>
2025-03-16 15:39:11 +0300

    güncellendi

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home:HomePage(),
+      );
+  }
+}
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
