16959915ef47c048cbb7c71aaededb199109fdc1
kaanertugrulkoc <poison_314@hotmail.com>
2025-03-16 19:15:48 +0300

    güncellendi

--- lib/widgets/buttonTurleri.dart
+++ lib/widgets/buttonTurleri.dart
@@ -10,12 +10,24 @@ class buttonTur extends StatelessWidget {
           backgroundColor: Colors.blueGrey.shade200,
           title: Text('Button Türleri'),
         ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}, child:Icon(Icons.access_alarm),),
         body: Center(
           child: Column(children: [
             TextButton(onPressed: () {}, child: Text("Alarm Kur")),
-            TextButton.icon(onPressed: () {},icon:Icon(Icons.access_alarm) ,label:  Text("Alarm Kur")),
-            ElevatedButton(onPressed: (){}, child:Text('Alarm Kur')),
-            ElevatedButton.icon(onPressed: (){}, icon:Icon(Icons.access_alarm) ,label:Text('Alarm Kur'))
+            TextButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.access_alarm),
+                label: Text("Alarm Kur")),
+            ElevatedButton(onPressed: () {}, child: Text('Alarm Kur')),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.access_alarm),
+                label: Text('Alarm Kur')),
+            OutlinedButton(onPressed: () {}, child: Text('Alarm Kur')),
+            OutlinedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.access_alarm),
+                label: Text('Alarm Kur'))
           ]),
         ),
       ),
