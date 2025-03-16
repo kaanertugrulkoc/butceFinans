815b80e40360f8626e40a429b36869a89c344529
kaanertugrulkoc <poison_314@hotmail.com>
2025-03-16 18:46:57 +0300

    güncellendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bitirme_projesi_app/buttonTurleri.dart';
+import 'package:bitirme_projesi_app/widgets/buttonTurleri.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
