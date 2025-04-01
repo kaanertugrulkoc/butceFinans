f2a3d9deef21d34f6ed5d02a25b7d2142cd9da27
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-01 20:38:28 +0300

    güncellendi

---
+++ lib/main_image.dart
@@ -0,0 +1,14 @@
+import 'package:bitirme_projesi_app/widgets/buttonTurleri.dart';
+import 'package:bitirme_projesi_app/widgets/imageWidget.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: imagewidget_(),
+    );
+  }
+}
