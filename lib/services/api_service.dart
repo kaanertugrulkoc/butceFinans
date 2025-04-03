876bd5aacfcf66a30dbf4f32c257906194c879b5
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-03 20:28:16 +0300

    güncellendi

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,11 +1,23 @@
 
 
 import 'package:bitirme_projesi_app/services/storage_service.dart';
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
+
+abstract class ApiConstants{
+  static const baseurl="";
+}
 
 class apiService extends GetxService {
   final StorageService _storageService = Get.find<StorageService>();
   late Dio _dio;
+
+  Future<ApiService> init() async{
+    _dio =Dio(BaseOptions(
+      baseUrl:ApiConstants.baseurl,
+
+    ));
+  }
 
 
 }
