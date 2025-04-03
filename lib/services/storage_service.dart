9f360b60c77ea7beb8e8e5555bb811cae978fa19
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-03 17:38:23 +0300

    güncellendi

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,8 +1,10 @@
 
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class StorageService extends GetxController{
 
+late final SharedPreferences _preferences;
 
 
 
