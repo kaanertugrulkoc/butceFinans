07cb06bbdb26f1781a3afe120ed7770f6f920ee5
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-03 17:40:30 +0300

    güncellendi

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,12 +1,10 @@
-
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class StorageService extends GetxController{
+class StorageService extends GetxController {
+  late final SharedPreferences _preferences;
 
-late final SharedPreferences _preferences;
+  Future<SharedPreferences> init() async {
 
-
-
-
+  }
 }
