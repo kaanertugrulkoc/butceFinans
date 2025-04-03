6830280991e7218d3c5f82d4d7424a74bb190909
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-03 22:20:54 +0300

    güncellendi

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,7 +2,6 @@ import 'package:bitirme_projesi_app/services/storage_service.dart';
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
-
 abstract class ApiConstants {
   static const baseurl = "";
 }
@@ -11,28 +10,26 @@ class ApiService extends GetxService {
   final StorageService _storageService = Get.find<StorageService>();
   late Dio _dio;
 
-  Future<ApiService>init() async {
+  Future<ApiService> init() async {
     _dio = Dio(BaseOptions(
         baseUrl: ApiConstants.baseurl,
         connectTimeout: Duration(seconds: 10),
         receiveTimeout: Duration(seconds: 10),
         contentType: "application/json"));
 
-    _dio.interceptors.add(InterceptorsWrapper(
-        onRequest: (options, handler) async {
-          final token = _storageService.getValue<String>(StorageKeys.UserToken);
-          if (token != null) {
-            options.headers['authorization'] = 'Bearer $token';
-          }
-          return handler.next(options);
-
-        },
-        onError: (error, handler) async {
-          if(error.response?.statusCode==401){
-            await _storageService.remove(StorageKeys.UserToken);
-          }
-          return handler.next(error);
-        }));
+    _dio.interceptors
+        .add(InterceptorsWrapper(onRequest: (options, handler) async {
+      final token = _storageService.getValue<String>(StorageKeys.UserToken);
+      if (token != null) {
+        options.headers['authorization'] = 'Bearer $token';
+      }
+      return handler.next(options);
+    }, onError: (error, handler) async {
+      if (error.response?.statusCode == 401) {
+        await _storageService.remove(StorageKeys.UserToken);
+      }
+      return handler.next(error);
+    }));
     return this;
   }
 }
