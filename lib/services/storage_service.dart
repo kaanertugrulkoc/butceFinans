4f0328e13d94a52cd854f96a6b930f12f093a155
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-03 18:02:36 +0300

    güncellendi

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -9,10 +9,16 @@ class StorageService extends GetxController {
     return _preferences;
   }
 
-  Future<void> setValue<T>(String key, T Value) async {
+  Future<bool> setValue<T>(String key, T Value) async {
     try {
       if (value is String) {
         return await _preferences.setString(key, value);
+      } else if (value is int) {
+        return await _preferences.setInt(key, value);
+      } else if (value is double) {
+        return await _preferences.setDouble(key, value);
+      } else if (value is bool) {
+        return await _preferences.setBool(key, value);
       }
     } catch (e) {}
   }
