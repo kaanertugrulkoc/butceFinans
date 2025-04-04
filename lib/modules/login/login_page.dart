76e515a23fe40128df23271d89727460799b94b7
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-04 08:15:58 +0300

    güncellendi

---
+++ lib/modules/login/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Login_Page extends StatelessWidget {
+  const Login_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child:Text('LoginPage'),),
+    );
+  }
+}
---
+++ lib/modules/splash/splash_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Splash_Page extends StatelessWidget {
+  const Splash_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child:Text('SplashPage'),),
+    );
+  }
+}
