ee31229a4df81dfe12542137cf2203d2a084652b
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-04 14:56:05 +0300

    güncellendi

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,10 +1,12 @@
 import 'package:bitirme_projesi_app/services/api_service.dart';
 import 'package:bitirme_projesi_app/services/storage_service.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService extends GetxService{
 
   late final StorageService _storageService;
   late final ApiService _apiService;
+  late final GoogleSignIn _googleSignIn;
 
 }
