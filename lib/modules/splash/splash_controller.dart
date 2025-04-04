1bd91f9347c25858e0adf9fda86dde974b93b20a
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-04 11:24:37 +0300

    güncellendi

--- lib/modules/splash/splash_controller.dart
+++ lib/modules/splash/splash_controller.dart
@@ -1,11 +1,11 @@
 import 'package:bitirme_projesi_app/core/base_controller.dart';
+import 'package:bitirme_projesi_app/services/storage_service.dart';
+import 'package:get/get.dart';
 
-class SplashController extends BaseController{
-
+class SplashController extends BaseController {
   @override
   void onReady() {
-
     super.onReady();
+    Get.find<StorageService>().getAllValues();
   }
-
 }
