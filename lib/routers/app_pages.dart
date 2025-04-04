3c77018c67218db608b4e19d435c58d0ab19d8f0
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-04 10:05:17 +0300

    güncellendi

--- lib/routers/app_pages.dart
+++ lib/routers/app_pages.dart
@@ -1,8 +1,15 @@
+import 'package:get/get.dart';
+
 abstract class AppRouters{
   static const INITIAL =SPLASH;
   static const SPLASH ='/splash';
   static const LOGIN ='/login';
   static const HOME ='/home';
   static const PROFILE ='/profil';
+}
 
+class AppPages{
+  static final pages =<GetPage>[
+    GetPage(name: name, page: page)
+  ];
 }
