3b73cfc86f906331b84f63f59c119812380d7cf6
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-04 11:56:01 +0300

    güncellendi

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:bitirme_projesi_app/routers/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+
 
   @override
   Widget build(BuildContext context) {
