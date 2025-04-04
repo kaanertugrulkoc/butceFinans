e71d591759ab1f44df779f81542e25a1801d0738
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-04 09:01:44 +0300

    güncellendi

---
+++ lib/modules/home/home_controller.dart
@@ -0,0 +1,6 @@
+
+import 'package:bitirme_projesi_app/core/base_controller.dart';
+
+class HomeController extends BaseController{
+
+}
---
+++ lib/modules/login/login_controller.dart
@@ -0,0 +1,5 @@
+import 'package:bitirme_projesi_app/core/base_controller.dart';
+
+class LoginController extends BaseController{
+
+}
