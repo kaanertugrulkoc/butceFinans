19f0a6ecfbc72eabeefe3794bdefe3cb548e2387
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-04 10:17:50 +0300

    güncellendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
+import 'package:bitirme_projesi_app/routers/app_pages.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('appbarTitle'),
-        ),
-        body: Center(
-
-        ),
+    return GetMaterialApp(
+      title: 'BÜTÇE',
+      getPages: AppPages.pages,
+      initialRoute: AppRouters.INITIAL,
     );
   }
 }
