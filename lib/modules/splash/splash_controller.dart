e46d7c090eae7e34422f8aa99bdc40d5c7446ab4
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-04 11:21:08 +0300

    güncellendi

---
+++ lib/modules/splash/splash_controller.dart
@@ -0,0 +1,11 @@
+import 'package:bitirme_projesi_app/core/base_controller.dart';
+
+class SplashController extends BaseController{
+
+  @override
+  void onReady() {
+
+    super.onReady();
+  }
+
+}
