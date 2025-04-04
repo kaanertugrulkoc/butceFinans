423a7512cdcb2e8dcafa52fcf529315ab6d55173
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-04 09:21:37 +0300

    güncellendi

--- lib/routers/app_pages.dart
+++ lib/routers/app_pages.dart
@@ -1,5 +1,8 @@
 abstract class AppRouters{
   static const INITIAL =SPLASH;
   static const SPLASH ='/splash';
+  static const LOGIN ='/login';
+  static const HOME ='/home';
+  static const PROFILE ='/profil';
 
 }
