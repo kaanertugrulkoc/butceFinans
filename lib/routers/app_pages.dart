a884db67e3ae1b5d4345831a77dbb6fb0389293a
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-04 10:11:49 +0300

    güncellendi

--- lib/routers/app_pages.dart
+++ lib/routers/app_pages.dart
@@ -2,16 +2,35 @@ import 'package:bitirme_projesi_app/modules/splash/splash_bindings.dart';
 import 'package:bitirme_projesi_app/modules/splash/splash_page.dart';
 import 'package:get/get.dart';
 
-abstract class AppRouters{
-  static const INITIAL =SPLASH;
-  static const SPLASH ='/splash';
-  static const LOGIN ='/login';
-  static const HOME ='/home';
-  static const PROFILE ='/profil';
+import '../modules/home/home_bindings.dart';
+import '../modules/home/home_page.dart';
+import '../modules/login/login_bindings.dart';
+import '../modules/login/login_page.dart';
+
+abstract class AppRouters {
+  static const INITIAL = SPLASH;
+  static const SPLASH = '/splash';
+  static const LOGIN = '/login';
+  static const HOME = '/home';
+  static const PROFILE = '/profil';
 }
 
-class AppPages{
-  static final pages =<GetPage>[
-    GetPage(name: AppRouters.SPLASH, page: ()=>SplashPage(),binding: SplashBindings())
+class AppPages {
+  static final pages = <GetPage>[
+    GetPage(
+      name: AppRouters.SPLASH,
+      page: () => SplashPage(),
+      binding: SplashBindings(),
+    ),
+    GetPage(
+      name: AppRouters.LOGIN,
+      page: () => LoginPage(),
+      binding: LoginBindings(),
+    ),
+    GetPage(
+      name: AppRouters.HOME,
+      page: () => HomePage(),
+      binding: HomeBindings(),
+    )
   ];
 }
