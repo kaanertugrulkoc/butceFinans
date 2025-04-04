109e9f6ea6f27b9224c71676db54b15e3ff239a4
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-04 09:25:49 +0300

    güncellendi

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('appbarTitle'),
+        ),
+        body: Center(
+
+        ),
+    );
+  }
+}
