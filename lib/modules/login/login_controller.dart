71ea486c75c6aa62a71a93e4b9d11dfb0083e6d0
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-05 08:19:14 +0300

    güncellendi

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,5 +1,19 @@
 import 'package:bitirme_projesi_app/core/base_controller.dart';
+import 'package:bitirme_projesi_app/services/auth_services.dart';
 
 class LoginController extends BaseController{
+late final AuthService _service;
+
+@override
+  void onInit() {
+
+    super.onInit();
+    _authservice = Get.find<AuthService>();
+  }
+
+  googleIleGirisYap()async{
+
+
+  }
 
 }
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,6 +1,8 @@
+import 'package:bitirme_projesi_app/modules/login/login_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends GetView<LoginController> {
   const LoginPage({super.key});
 
   @override
