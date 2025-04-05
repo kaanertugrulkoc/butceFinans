9e75b57a51e258a024677463c457030a03df62f9
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-05 13:04:02 +0300

    güncellendi

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,19 +1,16 @@
 import 'package:bitirme_projesi_app/core/base_controller.dart';
 import 'package:bitirme_projesi_app/services/auth_services.dart';
 
-class LoginController extends BaseController{
-late final AuthService _service;
+class LoginController extends BaseController {
+  late final AuthService _authService;
 
-@override
+  @override
   void onInit() {
-
     super.onInit();
-    _authservice = Get.find<AuthService>();
+    _authService = Get.find<AuthService>();
   }
 
-  googleIleGirisYap()async{
-
-
+  googleIleGirisYap() async {
+    await _authService.signInWithGoogle();
   }
-
 }
