307e4524d849425a6e468dd77e4ebeb8ee3846d2
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-05 08:11:43 +0300

    güncellendi

--- lib/modules/login/login_bindings.dart
+++ lib/modules/login/login_bindings.dart
@@ -1,8 +1,11 @@
+import 'package:bitirme_projesi_app/modules/login/login_controller.dart';
 import 'package:get/get.dart';
 
 class LoginBindings extends Bindings {
   @override
   void dependencies() {
-
+    Get.lazyPut(
+      () => LoginController(),
+    );
   }
 }
