0d55e902d9bf752b589c741b83322fa233dadf90
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-13 23:45:33 +0300

    güncellendi

--- lib/modules/profile/profile_controller.dart
+++ lib/modules/profile/profile_controller.dart
@@ -17,8 +17,8 @@ class ProfileController extends GetxController {
     try {
       isLoading.value = true;
       // TODO: Profil bilgilerini yükle
-      name.value = 'John Doe';
-      email.value = 'john.doe@example.com';
+      name.value = 'kaanertugrulkoc';
+      email.value = 'kaanertugrulkoc.com';
       phone.value = '+90 555 555 55 55';
       address.value = 'İstanbul, Türkiye';
     } catch (e) {
