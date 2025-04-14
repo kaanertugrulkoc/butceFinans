894990c172c822761926788429ce33d0c25fa3e1
kaanertugrulkoc <poison_314@hotmail.com>
2025-04-14 19:04:14 +0300

    güncellendi

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,12 @@ import 'services/storage_service.dart';
 import 'services/api_service.dart';
 import 'services/database_service.dart';
 import 'services/currency_service.dart';
+import '../controllers/income_controller.dart';
+import '../controllers/expense_controller.dart';
+import '../models/income.dart';
+import '../models/expense.dart';
+import 'pages/income_page.dart';
+import 'pages/expense_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -37,4 +43,129 @@ class MyApp extends StatelessWidget {
       getPages: AppPages.pages,
     );
   }
+}
+
+class IncomePage extends StatelessWidget {
+  final IncomeController controller = Get.put(IncomeController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gelirler'),
+      ),
+      body: Obx(() {
+        if (controller.incomes.isEmpty) {
+          return Center(child: Text('Henüz gelir yok.'));
+        }
+        return ListView.builder(
+          itemCount: controller.incomes.length,
+          itemBuilder: (context, index) {
+            final income = controller.incomes[index];
+            return ListTile(
+              title: Text(income.description),
+              subtitle: Text('₺${income.amount.toStringAsFixed(2)}'),
+            );
+          },
+        );
+      }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Gelir ekleme işlemi
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class ExpensePage extends StatelessWidget {
+  final ExpenseController controller = Get.put(ExpenseController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Giderler'),
+      ),
+      body: Obx(() {
+        if (controller.expenses.isEmpty) {
+          return Center(child: Text('Henüz gider yok.'));
+        }
+        return ListView.builder(
+          itemCount: controller.expenses.length,
+          itemBuilder: (context, index) {
+            final expense = controller.expenses[index];
+            return ListTile(
+              title: Text(expense.description),
+              subtitle: Text('₺${expense.amount.toStringAsFixed(2)}'),
+            );
+          },
+        );
+      }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Gider ekleme işlemi
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class IncomeController extends GetxController {
+  var incomes = <Income>[].obs;
+
+  void addIncome(Income income) {
+    incomes.add(income);
+  }
+
+  void fetchIncomes() {
+    // Veritabanından gelirleri çekme işlemi
+  }
+}
+
+class ExpenseController extends GetxController {
+  var expenses = <Expense>[].obs;
+
+  void addExpense(Expense expense) {
+    expenses.add(expense);
+  }
+
+  void fetchExpenses() {
+    // Veritabanından giderleri çekme işlemi
+  }
+}
+
+Widget _buildQuickActionsRow() {
+  return Padding(
+    padding: EdgeInsets.symmetric(horizontal: 16),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        _buildQuickActionButton(Icons.add, 'Gelirler', Colors.green, () {
+          Get.to(IncomePage());
+        }),
+        _buildQuickActionButton(Icons.remove, 'Giderler', Colors.red, () {
+          Get.to(ExpensePage());
+        }),
+      ],
+    ),
+  );
+}
+
+class AppPages {
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(name: Routes.HOME, page: () => HomePage()),
+    GetPage(name: Routes.INCOME, page: () => IncomePage()),
+    GetPage(name: Routes.EXPENSE, page: () => ExpensePage()),
+  ];
+}
+
+abstract class Routes {
+  static const HOME = '/home';
+  static const INCOME = '/income';
+  static const EXPENSE = '/expense';
 }
